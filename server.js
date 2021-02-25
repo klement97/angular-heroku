df819a0d6fae300d9e666b98b8b7b06a6cf2e0f7
Klement Omeri <klementomeri97@gmail.com>
2021-02-25 21:16:28 +0100

    ci: redirect all requests to https

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express');
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 
+const express = require('express');
 const app = express();
 
+app.use(requireHTTPS);
 app.use(express.static('./dist/angular-heroku'));
 
 app.get('/*', (req, res) =>
